b822abdafa6bba3032b1c1df7b3bc1ba216cbc92
Oliver Michels <oliver.michels@gmail.com>
2023-08-09 17:00:43 +0200

    added cobra framework

--- main.go
+++ main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
+
 	"os"
 )
 
 func main() {
 
-	key := os.Getenv("WEATHERAPI_KEY")
-	if key == "" {
-		fmt.Println("The environment variable WEATHERAPI_KEY is not set!\nSet this variable and try again.\n\nFor more informations read https://www.weatherapi.com/docs.")
-		os.Exit(1)
-	}
+	ExecuteRootCmd()
 
 	var location *string
 	location = flag.String("l", "", "Location for which the weather is to be shown")
@@ -23,22 +19,5 @@ func main() {
 		fmt.Println("The location is not set!")
 		os.Exit(0)
 	}
-
-	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + *location)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode >= 400 && res.StatusCode <= 410 {
-		printAPIError(res)
-		os.Exit(0)
-	}
-
-	if res.StatusCode != 200 {
-		panic("Weather API not available")
-	}
-
-	printWeather(res)
 
 }
