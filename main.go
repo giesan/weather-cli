05f5410748337c90c858c406f26437a0906b336f
Oliver Michels <oliver.michels@gmail.com>
2023-08-09 17:05:05 +0200

    rebase

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-
-	"os"
-)
-
 func main() {
-
 	ExecuteRootCmd()
-
-	var location *string
-	location = flag.String("l", "", "Location for which the weather is to be shown")
-	flag.Parse()
-
-	if *location == "" {
-		fmt.Println("The location is not set!")
-		os.Exit(0)
-	}
-
 }
